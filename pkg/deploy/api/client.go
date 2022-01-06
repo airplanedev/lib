3841b3f5c68441b01ac40dfa54cc4fcd0891d4aa
leeweisberger <lweisberger5@gmail.com>
2022-01-06 08:22:42 -0800

    remove comments

--- pkg/deploy/api/client.go
+++ pkg/deploy/api/client.go
@@ -12,13 +12,7 @@ import (
 type APIClient interface {
 	GetTask(ctx context.Context, slug string) (res Task, err error)
 	ListResources(ctx context.Context) (res ListResourcesResponse, err error)
-	// UpdateTask(ctx context.Context, req UpdateTaskRequest) (res UpdateTaskResponse, err error)
-	// CreateTask(ctx context.Context, req CreateTaskRequest) (res CreateTaskResponse, err error)
-	// CreateBuild(ctx context.Context, req CreateBuildRequest) (res CreateBuildResponse, err error)
-	// GetRegistryToken(ctx context.Context) (res RegistryTokenResponse, err error)
 	CreateBuildUpload(ctx context.Context, req CreateBuildUploadRequest) (res CreateBuildUploadResponse, err error)
-	// GetBuildLogs(ctx context.Context, buildID string, prevToken string) (res GetBuildLogsResponse, err error)
-	// GetBuild(ctx context.Context, id string) (res GetBuildResponse, err error)
 }
 
 // Task represents a task.
