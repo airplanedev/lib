cf46188a88d165575f32632694fbc064c08646d0
leeweisberger <lweisberger5@gmail.com>
2022-01-11 11:13:00 -0500

    add back panic

--- pkg/runtime/runtime.go
+++ pkg/runtime/runtime.go
@@ -9,6 +9,7 @@ package runtime
 
 import (
 	"context"
+	"fmt"
 	"io"
 	"os"
 	"path/filepath"
@@ -105,6 +106,9 @@ var runtimes = make(map[string]Interface)
 
 // Register registers the given ext with r.
 func Register(ext string, r Interface) {
+	if _, ok := runtimes[ext]; ok {
+		panic(fmt.Sprintf("runtime: %s already registered", ext))
+	}
 	runtimes[ext] = r
 }
 
