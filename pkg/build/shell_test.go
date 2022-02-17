81b39516d4c79aabdea856456d49472881b5c5a0
leeweisberger <lee@airplane.dev>
2022-02-17 14:05:26 -0500

    test workdir

--- pkg/build/shell_test.go
+++ pkg/build/shell_test.go
@@ -9,31 +9,39 @@ func TestShellBuilder(t *testing.T) {
 	ctx := context.Background()
 
 	tests := []Test{
+		// {
+		// 	Root: "shell/simple",
+		// 	Kind: TaskKindShell,
+		// 	Options: KindOptions{
+		// 		"shim":       "true",
+		// 		"entrypoint": "main.sh",
+		// 	},
+		// },
+		// {
+		// 	Root: "shell/ubuntu",
+		// 	Kind: TaskKindShell,
+		// 	Options: KindOptions{
+		// 		"shim":       "true",
+		// 		"entrypoint": "main.sh",
+		// 	},
+		// 	SearchString: "bar",
+		// },
+		// {
+		// 	Root: "shell/zcli",
+		// 	Kind: TaskKindShell,
+		// 	Options: KindOptions{
+		// 		"shim":       "true",
+		// 		"entrypoint": "main.sh",
+		// 	},
+		// 	SearchString: "bar",
+		// },
 		{
-			Root: "shell/simple",
+			Root: "shell/diffworkdir",
 			Kind: TaskKindShell,
 			Options: KindOptions{
 				"shim":       "true",
 				"entrypoint": "main.sh",
 			},
-		},
-		{
-			Root: "shell/ubuntu",
-			Kind: TaskKindShell,
-			Options: KindOptions{
-				"shim":       "true",
-				"entrypoint": "main.sh",
-			},
-			SearchString: "bar",
-		},
-		{
-			Root: "shell/zcli",
-			Kind: TaskKindShell,
-			Options: KindOptions{
-				"shim":       "true",
-				"entrypoint": "main.sh",
-			},
-			SearchString: "bar",
 		},
 	}
 
---
+++ pkg/examples/shell/diffworkdir/main.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Linked to https://app.airplane.dev/t/new_shell_task [do not edit this line]
+# Params are in environment variables as PARAM_{SLUG}, e.g. PARAM_USER_ID
+
+echo "hello"
