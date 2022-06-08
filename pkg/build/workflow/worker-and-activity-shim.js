af9ad1f4e5dae38652da43acc5995d9a8dac6c4c
Justin Lu <justin@airplane.dev>
2022-06-07 18:25:15 -0700

    path

--- pkg/build/workflow/worker-and-activity-shim.js
+++ pkg/build/workflow/worker-and-activity-shim.js
@@ -4,7 +4,7 @@ import { NativeConnection, Worker } from '@temporalio/worker';
 //
 // TODO: Make this path configurable.
 
-import * as activities from '../activities/activities.js';
+import * as activities from '../activities';
 
 // import * as sdkActivities from 'airplane/activities'
 // const activities = {
