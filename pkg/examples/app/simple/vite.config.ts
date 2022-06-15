ca37d08943c96074d1b9514b713c831523e04455
leeweisberger <lee@airplane.dev>
2022-06-15 16:27:53 -0400

    Override base when building

--- pkg/examples/app/simple/vite.config.ts
+++ pkg/examples/app/simple/vite.config.ts
@@ -1,11 +1,17 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv, ResolvedConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "",
-  build: {
-    assetsDir: "",
-  },
-});
+export default ({ command, mode }: ResolvedConfig) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  const host = process.env.AIRPLANE_API_HOST || "https://api.airplane.so:5000";
+  const base = command === "serve" ? "" : `${host}/i/views/getContent/`;
+  return defineConfig({
+    plugins: [react()],
+    base,
+    envPrefix: "AIRPLANE_",
+    build: {
+      assetsDir: "",
+    },
+  });
+};
