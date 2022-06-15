db6daed200ca305a713601f0c54928d0b553cea9
leeweisberger <lee@airplane.dev>
2022-06-15 18:33:32 -0400

    Add view id to url

--- pkg/examples/app/simple/vite.config.ts
+++ pkg/examples/app/simple/vite.config.ts
@@ -4,8 +4,10 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default ({ command, mode }: ResolvedConfig) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  const viewID = process.env.AIRPLANE_VIEW_ID;
   const host = process.env.AIRPLANE_API_HOST || "https://api.airplane.dev";
-  const base = command === "build" ? `${host}/i/views/getContent/` : "";
+  const base =
+    command === "build" ? `${host}/i/views/getContent/${viewID}/` : "";
   return defineConfig({
     plugins: [react()],
     base,
