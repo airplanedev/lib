a7bbacfa8963c9d463646f1ab975704c73c0a3e9
leeweisberger <lee@airplane.dev>
2022-06-15 16:29:10 -0400

    default host

--- pkg/examples/app/simple/vite.config.ts
+++ pkg/examples/app/simple/vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default ({ command, mode }: ResolvedConfig) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
-  const host = process.env.AIRPLANE_API_HOST || "https://api.airplane.so:5000";
+  const host = process.env.AIRPLANE_API_HOST || "https://api.airplane.dev";
   const base = command === "build" ? `${host}/i/views/getContent/` : "";
   return defineConfig({
     plugins: [react()],
