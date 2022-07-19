300d25f317f77946f5cc287e8e1e68d701c6b64d
Justin Lu <justin@airplane.dev>
2022-07-19 16:38:11 -0700

    only clean in v1

--- pkg/build/node.go
+++ pkg/build/node.go
@@ -171,12 +171,13 @@ func node(root string, options KindOptions, buildArgs []string) (string, error) 
 			// is no real replacement for these, so we'll just install with `yarn install`.
 			installCommand = "yarn install"
 		} else {
-			installCommand = "yarn install --non-interactive --production --frozen-lockfile"
+			// Because the install command is running in the context of a docker build, the yarn cache
+			// isn't used after the packages are installed, and so we clean the cache to keep the
+			// image lean. This doesn't apply to Yarn v2 (specifically Plug'n'Play), which uses the
+			// cache directory for storing packages.
+			installCommand = "yarn install --non-interactive --production --frozen-lockfile && yarn cache clean"
 		}
-		// Because the install command is running in the context of a docker build, the yarn cache
-		// isn't used after the packages are installed, and so we clean the cache to keep the
-		// image lean.
-		installCommand += " && yarn cache clean"
+
 	} else if hasPackageLock {
 		// Use npm ci if possible, since it's faster and behaves better:
 		// https://docs.npmjs.com/cli/v8/commands/npm-ci
